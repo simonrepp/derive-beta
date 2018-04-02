2c85d0443b54cd702338e5178c26e3a339faa366
Simon Repp <simon@fdpl.io>
2018-04-02 14:41:26 +0200

    Additions

--- derive.at/write-media.js
+++ derive.at/write-media.js
@@ -1,7 +1,7 @@
 const fsExtra = require('fs-extra'),
       path = require('path'),
       sharp = require('sharp');
-      
+
 module.exports = async data => {
 
   const copy = (fromRelative, toRelative) => {
@@ -20,37 +20,39 @@ module.exports = async data => {
                               .withoutEnlargement()
                               .toFile(toAbsolute);
   };
-  
+
   const concurrentWrites = [];
-  
-  // TODO: In addition to the !written check, each media should also be stats()'ed and 
+
+  // TODO: In addition to the !written check, each media should also be stats()'ed and
   //       the results compared against cached stats, if mTime or size differ we rewrite.
   //       We might also want to include a ?3350350530 postfix to the written url to
   //       invalidate the browser cache
   //       (re stat check, we could possibly do this in connectMedia too)
+  //
+  //       if(article.image && !article.image.written) ... what we used but does not work like that
 
   for(let article of data.articles.values()) {
-    if(article.image && !article.image.written) {
+    if(article.image) {
       const finalPath = path.join('/texte', article.permalink, `bild${path.extname(article.image.connected)}`);
       article.image.written = finalPath;
-      
+
       concurrentWrites.push( copyResized(article.image.connected, finalPath) );
     }
 
-    if(article.text && !article.text.written) {
+    if(article.text) {
       let written = article.text.connected;
 
       for(let [originalFilePath, replacedFilePath] of article.text.downloads) {
         const finalPath = path.join('/texte', article.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copy(originalFilePath, finalPath) );
       }
 
       for(let [originalFilePath, replacedFilePath] of article.text.embeds) {
         const finalPath = path.join('/texte', article.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copyResized(originalFilePath, finalPath) );
       }
 
@@ -59,36 +61,36 @@ module.exports = async data => {
   }
 
   for(let book of data.books.values()) {
-    if(book.cover && !book.cover.written) {
+    if(book.cover) {
       const finalPath = path.join('/bücher', book.permalink, `cover${path.extname(book.cover.connected)}`);
       book.cover.written = finalPath;
-      
+
       concurrentWrites.push( copyResized(book.cover.connected, finalPath) );
     }
   }
 
   for(let event of data.events.values()) {
-    if(event.image && !event.image.written) {
+    if(event.image) {
       const finalPath = path.join('/veranstaltungen', event.permalink, `bild${path.extname(event.image.connected)}`);
       event.image.written = finalPath;
-      
+
       concurrentWrites.push( copyResized(event.image.connected, finalPath) );
     }
 
-    if(event.text && !event.text.written) {
+    if(event.text) {
       let written = event.text.connected;
 
       for(let [originalFilePath, replacedFilePath] of event.text.downloads) {
         const finalPath = path.join('/veranstaltungen', event.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copy(originalFilePath, finalPath) );
       }
 
       for(let [originalFilePath, replacedFilePath] of event.text.embeds) {
         const finalPath = path.join('/veranstaltungen', event.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copyResized(originalFilePath, finalPath) );
       }
 
@@ -97,10 +99,10 @@ module.exports = async data => {
   }
 
   for(let issue of data.issues.values()) {
-    if(issue.cover && !issue.cover.written) {
+    if(issue.cover) {
       const finalPath = path.join('/zeitschrift', issue.number.toString(), `cover${path.extname(issue.cover.connected)}`);
       issue.cover.written = finalPath;
-      
+
       concurrentWrites.push( copyResized(issue.cover.connected, finalPath) );
     }
   }
@@ -114,60 +116,60 @@ module.exports = async data => {
   // for(let page of data.pages.values()) {
   //   if(page.urbanize === null && page.text) {
   //     let written = page.text.connected;
-  // 
+  //
   //     for(let [originalFilePath, replacedFilePath] of page.text.downloads) {
   //       const finalPath = path.join('/seiten', page.permalink, replacedFilePath);
   //       written = written.replace(replacedFilePath, finalPath);
-  
+
   //       concurrentWrites.push( copy(originalFilePath, finalPath) );
   //     }
-  // 
+  //
   //     for(let [originalFilePath, replacedFilePath] of page.text.embeds) {
   //       const finalPath = path.join('/seiten', page.permalink, replacedFilePath);
   //       written = written.replace(replacedFilePath, finalPath);
-  
+
   //       concurrentWrites.push( copyResized(originalFilePath, finalPath) );
   //     }
-  // 
+  //
   //     page.text.written = written;
   //   }
   // }
 
   for(let program of data.programs.values()) {
-    if(program.image && !program.image.written) {
+    if(program.image) {
       const finalPath = path.join('/radio', program.permalink, `bild${path.extname(program.image.connected)}`);
       program.image.written = finalPath;
-      
+
       concurrentWrites.push( copyResized(program.image.connected, finalPath) );
     }
 
-    if(program.soundfile && !program.soundfile.written) {
+    if(program.soundfile) {
       const finalPath = path.join('/radio', program.permalink, `aufnahme${path.extname(program.soundfile.connected)}`);
       program.soundfile.written = finalPath;
-      
+
       concurrentWrites.push( copy(program.soundfile.connected, finalPath) );
     }
 
-    if(program.text && !program.text.written) {
+    if(program.text) {
       let written = program.text.connected;
 
       for(let [originalFilePath, replacedFilePath] of program.text.downloads) {
         const finalPath = path.join('/radio', program.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copy(originalFilePath, finalPath) );
       }
 
       for(let [originalFilePath, replacedFilePath] of program.text.embeds) {
         const finalPath = path.join('/radio', program.permalink, replacedFilePath);
         written = written.replace(replacedFilePath, finalPath);
-        
+
         concurrentWrites.push( copyResized(originalFilePath, finalPath) );
       }
 
       program.text.written = written;
     }
   }
-  
+
   await Promise.all(concurrentWrites);
 };
--- reference/markdown.md
+++ reference/markdown.md
@@ -1,9 +1,68 @@
+<!--
+  Das ist das Markdown Handbuch für die dérive Website.
+
+  Das Handbuch enthält sämtliche Markdown Features die wir verwenden,
+  und dient somit als praktisches Nachschlagewerk dass beim verfassen
+  von neuen Markdowntexten hilft.
+
+  In zweiter Linie dient es auch als Bindeglied zwischen der inhaltlichen
+  und der technischen Umsetzung der Seite - Bei der technischen Umsetzung der
+  Websites kann die ProgrammiererIn hier nachsehen welche Markdown Features
+  die Website unterstützen muss, und umgekehrt können sich alle Verfasser von
+  Inhalten darauf verlassen, dass diese dann auch korrekt dargestellt werden.
+
+  Das Handbuch ist selbst als Markdown Dokument verfasst, in der oberen
+  Menüleiste von Atom unter "Packages" -> "Markdown Preview" -> "Toggle Preview"
+  kann ein Markdown Preview Panel geöffnet werden, dessen Inhalt sich
+  automatisch aktualisiert wenn man hier in diesem Dokument Dinge verändert
+  und ausprobiert - was erlaubt und erwünscht ist!
+-->
+
+*Kursiver Text*
+
+**Fetter Text**
+
 # Erste Überschrift
+
 ## Zweite Überschrift
+
 ### Dritte Überschrift
 
-*Kursiv*
-**Fett**
+<https://beispiel.com/website/link/ohne/beschreibung>
 
-[Link](www.link.com)
-![Bild](/bild.jpg)
+<email.link@beispiel.com>
+
+[Link Beschreibung](www.beispiel.com)
+
+![Bildbeschreibung für Screenreader](/Pfad/eines/bildes.jpg "Hover Text")
+
+Ein Stern im Text der nicht als Beginn eines kursiven Textes interpretiert
+werden soll muss mit einem Backslash davor versehen werden: \*
+
+
+<!--
+  Anmerkung: Die folgenden Features werden im Markdown Preview Panel von Atom
+             nicht korrekt dargestellt, da sie weniger stark verbreiteten
+             Markdown Standards enstammen. Auf der Website werden sie aber -
+             wie alle anderen Features oberhalb auch - voll unterstützt!
+-->
+
+
+<!-- Vimeo Embed: -->
+
+<iframe src="http://player.vimeo.com/video/123456789" width="500" height="281" frameborder="0" webkitAllowFullScreen="webkitAllowFullScreen" mozallowfullscreen="mozallowfullscreen" allowFullScreen="allowFullScreen"></iframe>
+
+
+<!-- Youtube Embed: -->
+
+<iframe width="640" height="360" src="http://www.youtube.com/embed/xCpmfBWRPPM?feature=player_detailpage" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+
+
+<!-- Bibliographische Verweise bzw. Fussnoten: -->
+
+Ein bibliographischer Quellenhinweis[^1] innerhalb eines Textes.
+
+(...)
+
+[^1]:
+   64:     Siehe: <http://www.beispiel.com/quelle> (Stand 25.02.2009)
