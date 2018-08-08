6921e2318f497b2ae889591e778b0262a8be0eef
Simon Repp <simon@fdpl.io>
2018-08-08 11:37:21 +0200

    Dyamic search tile progress

--- derive.at/scripts/search.js
+++ derive.at/scripts/search.js
@@ -27,9 +27,9 @@ const authorResult = function(author) {
   let html = '';
 
   html += '<div class="tile">';
-  html += '  <h1>';
+  html += '  <div class="tile_header">';
   html += '    <a href="/autoren/' + author.permalink + '/">' + author.name + '</a>';
-  html += '  </h1>';
+  html += '  </div>';
   if(author.biography) {
     html += '<strong>' + author.biography.converted + '</strong>';
   }
@@ -42,21 +42,68 @@ const bookResult = function(book) {
   let html = '';
 
   html += '<div class="tile">';
-  html += '  <h1>';
+  html += '  <div class="tile_header">';
   html += '    <a href="/bücher/' + book.permalink + '/">' + book.title + '</a>';
-  html += '  </h1>';
+  html += '  </div>';
+  html += '  <div class="tile_image_split">';
+  html += '    <div class="tile_image_split__image">';
+  if(book.cover) {
+  html += '      <img src="' + book.cover.written +'" />';
+  }
+  html += '    </div>';
+  html += '    <div class="tile_image_split__meta">';
+  html +=        book.authors.map(function(author) {
+                   return '<a class="generic__smaller_text" href="/autoren/' + author.permalink + '/">' + author.name + '</a>';
+                 }).join(', ');
+
+  html += '      <div class="generic__margin-vertical">';
+  html += [
+    book.placeOfPublication ? book.placeOfPublication + ':' : '',
+    book.publishers.map(function(publisher) { return '<a href="/verlage/' + publisher.permalink + '/">' + publisher.name + '</a>'; }).join(', '),
+    book.yearOfPublication ? '(' + book.yearOfPublication + ')' : ''
+  ].join(' ').trim()
+  html += '      </div>';
+
+  if(book.reviews.length > 1) {
+    html += '    Rezensionen lesen: ' + book.reviews.map(function(review, index) { return '<a href="/texte/' + review.permalink + '/">' + (index + 1) + '</a>'; }).join(' ');
+  } else if(book.reviews.length === 1) {
+    html += '    <a href="/texte/' + book.reviews[0].permalink + '/">Rezension lesen</a>';
+  }
+  html += '    </div>';
+  html += '  </div>';
   html += '</div>';
 
   return html;
 }
 
+const formattedQuarter = {
+  1: 'Jän - Mär',
+  2: 'Apr - Juni',
+  3: 'Juli - Sept',
+  4: 'Okt - Dez'
+};
+
 const issueResult = function(issue) {
   let html = '';
 
   html += '<div class="tile">';
-  html += '  <h1>';
+  html += '  <div class="tile_header">';
+  html += '    <a href="/zeitschrift/' + issue.number + '/">dérive N°' + issue.number + '</a>';
+  html += '  </div>';
+  html += '  <div class="generic__subheading">';
   html += '    <a href="/zeitschrift/' + issue.number + '/">' + issue.title + '</a>';
-  html += '  </h1>';
+  html += '  </div>';
+  html += '  <div class="tile_image_split">';
+  html += '    <div class="tile_image_split__image">';
+  html += '      <img src="' + issue.cover.written +'" />';
+  html += '    </div>';
+  html += '    <div class="tile_image_split__meta">';
+  html +=        formattedQuarter[issue.quarter] + ' / ' + issue.year + '<br/>';
+  if(issue.outOfPrint) {
+    html +=        'Vergriffen!';
+  }
+  html += '    </div>';
+  html += '  </div>';
   html += '</div>';
 
   return html;
@@ -66,9 +113,32 @@ const programResult = function(program) {
   let html = '';
 
   html += '<div class="tile">';
-  html += '  <h1>';
+  html += '  <div class="tile_header">';
   html += '    <a href="/radio/' + program.permalink + '/">' + program.title + '</a>';
-  html += '  </h1>';
+  html += '  </div>';
+  html += '  <div class="tile_image_split">';
+  html += '    <div class="tile_image_split__image">';
+  if(program.image) {
+  html += '      <img src="' + program.image.written +'" />';
+  }
+  html += '    </div>';
+  html += '    <div class="tile_image_split__meta">';
+  if(program.subtitle) {
+  html += '<strong><a href="/radio/' + program.permalink + '/">' + program.subtitle + '</a></strong>'
+  }
+  html += '      <div class="generic__margin-vertical">';
+  html += '      <strong>Redaktion</strong><br/>';
+  html +=        program.editors.map(function(editor) {
+                   return '<a href="/autoren/' + editor.permalink + '/">' + editor.name + '</a>';
+                 }).join(', ');
+  html += '      </div>';
+
+  html += '      <div class="generic__margin-vertical">';
+  html += '        <strong>Erstaustrahlung</strong><br/>';
+  html +=          "TODO moment(program.firstBroadcast).locale('de').format('Do MMMMM YYYY')";
+  html += '      </div>';
+  html += '    </div>';
+  html += '  </div>';
   html += '</div>';
 
   return html;
@@ -122,4 +192,4 @@ const renderSearch = function() {
 
 renderSearch();
 
-document.addEventListener("turbolinks:render", renderSearch);
+document.addEventListener('turbolinks:render', renderSearch);
