6a544adc179233551e664e679f21bec1bee0ac53
Simon Repp <simon@fdpl.io>
2018-09-19 16:04:40 +0200

    Urbanize Berlin responsive corrections

--- urbanize.at/widgets/header.js
+++ urbanize.at/widgets/header.js
@@ -1,11 +1,9 @@
 module.exports = `
   <div class="header offset">
-    <div>
-      <a class="brand" href="/">&nbsp;bewegung.<br/>&nbsp;macht.<br/>&nbsp;stadt.</a>
-    </div>
+    <a class="brand" href="/">bewegung.<br/>macht.<br/>stadt.</a>
 
-    <div style="text-align: right;">
-      <span style="font-size: 1.65em;">urbanize!</span><br/>
+    <div class="header_info">
+      <span class="header_urbanize">urbanize!</span><br/>
       Int. festival für urbane erkundungen<br/><br/>
       5.-14. okt. 2018<br/>
       berlin
