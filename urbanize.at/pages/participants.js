ffa655a7dd973e34de324d5d52117cc5a22048ba
Simon Repp <simon@fdpl.io>
2018-09-20 19:11:52 +0200

    Veranstalter*innen

--- urbanize.at/pages/participants.js
+++ urbanize.at/pages/participants.js
@@ -5,7 +5,7 @@ module.exports = urbanize => {
   const html = `
     <div>
       <div class="generic__heading">
-        VeranstalterInnen
+        Veranstalter*innen
       </div>
 
       ${participantListing(urbanize.hosts)}
