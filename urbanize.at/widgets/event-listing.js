f420c9145763f36ff85af73cebb061d9d2b25585
Simon Repp <simon@fdpl.io>
2018-09-21 14:42:16 +0200

    Sort urbanize event listings by date and time

--- urbanize.at/widgets/event-listing.js
+++ urbanize.at/widgets/event-listing.js
@@ -1,8 +1,40 @@
 const timeframe = require('./timeframe.js');
 
+const eventSort = (a, b) => {
+  let aDate, bDate;
+
+  if(a.dates && a.dates.length > 0) {
+    aDate = a.dates[0].date;
+  }
+
+  if(b.dates && b.dates.length > 0) {
+    bDate = b.dates[0].date;
+  }
+
+  const dateDifference = aDate - bDate
+
+  if(dateDifference === 0) {
+    let aTime, bTime;
+
+    if(a.dates && a.dates.length > 0) {
+      aTime = a.dates[0].time;
+    }
+
+    if(b.dates && b.dates.length > 0) {
+      bTime = b.dates[0].time;
+    }
+
+    console.log(aTime, bTime, aTime > bTime ? 1 : -1);
+
+    return aTime > bTime ? 1 : -1;
+  } else {
+    return dateDifference;
+  }
+};
+
 module.exports = events => `
   <div>
-    ${events.map(event => `
+    ${events.sort(eventSort).map(event => `
       <div class="list-item">
         ${event.image ? `
           <img class="teaser-image" src="${event.image.written}">
