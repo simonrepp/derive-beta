1e3b7ee39f0bb226d790881c7f95a0c650a85332
Simon Repp <simon@fdpl.io>
2018-11-10 15:33:48 +0100

    Clip and scroll feature text, heavily improve feature tiling logic

--- derive.at/pages/index.js
+++ derive.at/pages/index.js
@@ -6,27 +6,30 @@ module.exports = data => {
 
   const html = `
     <div class="features">
+
       ${sortedFeatures.map(feature => `
-        <div class="feature_split feature__${feature.type}">
-          <div class="feature_image"
-               ${feature.image ? ` style="background-image: url(${feature.image.written});"` : ''} >
-          </div>
-
-          <div class="feature_text">
-            ${feature.header ? `
-              <div class="generic__serif">
-                ${feature.header}
-              </div>
-            `:''}
-
-            <div class="generic__heading">
-              ${feature.url ? `<a href="${feature.url}">${feature.title}</a>` : feature.title}
+        <div class="feature__${feature.type}">
+          <div class="feature_split">
+            <div class="feature_image" ${feature.image ? ` style="background-image: url(${feature.image.written});"` : ''} >
             </div>
 
-            ${feature.text ? `<div class="generic__serif">${feature.text.converted}</div>` : ''}
+            <div class="feature_text">
+              ${feature.header ? `
+                <div class="generic__serif">
+                  ${feature.header}
+                </div>
+              `:''}
+
+              <div class="generic__heading">
+                ${feature.url ? `<a href="${feature.url}">${feature.title}</a>` : feature.title}
+              </div>
+
+              ${feature.text ? `<div class="generic__serif">${feature.text.converted}</div>` : ''}
+            </div>
           </div>
         </div>
       `).join('')}
+
     </div>
   `;
 
