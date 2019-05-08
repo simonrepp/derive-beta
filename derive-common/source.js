2a132ed7121c840c256ef740431ff6278e92ac11
Simon Repp <simon@fdpl.io>
2019-05-08 16:07:47 +0200

    Exclude new archive directory during globbing, compact source module

--- derive-common/source.js
+++ derive-common/source.js
@@ -28,77 +28,34 @@ module.exports = async data => {
   data.programs.clear();
   data.warnings = [];
 
-  const globPaths = await glob('**/*', { cwd: data.root, onlyFiles: true });
+  const globPaths = await glob(['**/*', '!Archiv'], { cwd: data.root, onlyFiles: true });
 
   for(let localFilesystemPath of globPaths) {
     const normalizedPath = localFilesystemPath.normalize();
 
     if(normalizedPath.match(NO_EXTENSION)) {
-
       data.warnings.push({
         files: [{ path: localFilesystemPath }],
         message: `Die Datei ${normalizedPath} hat keine Dateiendung.`,
       });
-
     } else if(normalizedPath.match(FORBIDDEN_FILENAME_CHARACTERS)) {
-
       data.warnings.push({
         files: [{ path: localFilesystemPath }],
         message: 'Im Namen der Datei bzw. des Ordners wurde eines der nicht erlaubten Zeichen  / \\ ? * : | " < > vorgefunden; Die nicht erlaubten Zeichen sollten entfernt bzw. durch Leerzeichen oder alternative Zeichen wie "_" oder "-" ersetzt werden.',
       });
-
     } else if(path.extname(normalizedPath) === '.eno') {
-
-      if(normalizedPath === 'Festival/Festival.eno') {
-
-        await sourceFestival(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Akteure\//)) {
-
-        await sourcePlayer(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Bücher\//)) {
-
-        await sourceBook(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Features\//)) {
-
-        await sourceFeature(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Kino\//)) {
-
-        await sourceScreening(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Radio\//)) {
-
-        await sourceProgram(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Seiten\//)) {
-
-        await sourcePage(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Texte\//)) {
-
-        await sourceArticle(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Veranstaltungen\//)) {
-
-        await sourceEvent(data, localFilesystemPath);
-
-      } else if(normalizedPath.match(/^Zeitschriften\//)) {
-
-        await sourceIssue(data, localFilesystemPath);
-
-      }
-
-    } else if(!normalizedPath.match(/^\.|^Dokumentation\//)) {
-      const media = {
-        localFilesystemPath: localFilesystemPath,
-        used: false
-      };
-
-      data.media.set(normalizedPath, media);
-
+      if(normalizedPath === 'Festival/Festival.eno') { await sourceFestival(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Akteure\//)) { await sourcePlayer(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Bücher\//)) { await sourceBook(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Features\//)) { await sourceFeature(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Kino\//)) { await sourceScreening(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Radio\//)) { await sourceProgram(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Seiten\//)) { await sourcePage(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Texte\//)) { await sourceArticle(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Veranstaltungen\//)) { await sourceEvent(data, localFilesystemPath); } else
+      if(normalizedPath.match(/^Zeitschriften\//)) { await sourceIssue(data, localFilesystemPath); }
+    } else {
+      data.media.set(normalizedPath, { localFilesystemPath, used: false });
     }
   }
 
