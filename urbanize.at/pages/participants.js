e82be3d05688315b3d463f349dedb72eced9e8da
Simon Repp <simon@fdpl.io>
2019-08-08 16:19:35 +0200

    Flesh out new header, footer and participants page

--- urbanize.at/pages/participants.js
+++ urbanize.at/pages/participants.js
@@ -1,14 +1,35 @@
 const layout = require('./layout.js');
 
 module.exports = urbanize => {
+  const participantsByLetter = [];
+
+  for(const participant of urbanize.participants) {
+    const letter = participant.name[0];
+
+    if(participantsByLetter.hasOwnProperty(letter)) {
+      participantsByLetter[letter].push(participant);
+    } else {
+      participantsByLetter[letter] = [participant];
+    }
+  }
+
   const html = `
-    <div>
+    <div class="participants">
       <div class="generic__heading">
-        Beteiligte
+        Beteiligte:
       </div>
 
-      ${urbanize.participants.map(participant => `
-        <div><a href="/${participant.permalink}/">${participant.name}</a></div>
+      ${Object.entries(participantsByLetter).map(([letter, participants]) => `
+        <div class="letter">
+          <div>
+            ${letter}
+          </div>
+          <div>
+            ${participants.map(participant => `
+              <div><a href="/${participant.permalink}/">${participant.name}</a></div>
+            `).join('')}
+          </div>
+        </div>
       `).join('')}
     </div>
   `;
--- urbanize.at/widgets/footer.js
+++ urbanize.at/widgets/footer.js
@@ -1,9 +1,33 @@
 module.exports = urbanize => `
   <div class="footer">
-    2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN<br><br>
+    <div>
+    <a class="button_rectangle" href="#">Newsletter</a><br>
+    <a class="button_rectangle" href="#">Impressum</a><br>
+    </div>
 
-    <a href="https://2018.urbanize.at">Archiv (Links zu vorherigen Festivals)</a><br>
-    <a href="#">facebook</a><br>
-    <a href="#">twitter</a><br>
-    <a href="#">instagram</a><br>
+    <div>
+      dérive<br>
+      dérive — Verein für Stadtforschung<br>
+      <a href="https://derive.at">derive.at</a><br><br>
+
+      <a class="button_circle" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
+      <a class="button_circle" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
+      <a class="button_circle" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
+      <a class="button_circle" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
+      <a class="button_circle" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
+
+      © 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN
+    </div>
+
+    <div>
+      Festivalarchiv<br>
+      <a href="https://2018.urbanize.at/">ur9anize! 2018</a><br>
+      <a href="https://2017.urbanize.at/">ur8anize! 2017</a><br>
+      <a href="https://2016.urbanize.at/">urbani7e! 2016</a><br>
+      <a href="https://2015.urbanize.at/">ur6anize! 2015</a><br>
+      <a href="https://2014.urbanize.at/">ur5anize! 2014</a><br>
+      <a href="https://2013.urbanize.at/">ur4anize! 2013</a><br>
+      <a href="https://2012.urbanize.at/">ur3anize! 2012</a><br>
+      <a href="https://2011.urbanize.at/">urbani2e! 2011</a><br>
+    </div>
   </div>`;
