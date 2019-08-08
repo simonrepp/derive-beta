6de058dda6565723eaf843a53a46a02964fbf955
Simon Repp <simon@fdpl.io>
2019-08-08 14:44:13 +0200

    Integrate new urbanize favicon

--- urbanize.at/pages/layout.js
+++ urbanize.at/pages/layout.js
@@ -39,13 +39,7 @@ module.exports = (content, urbanize, options = {}) => {
         <meta name="turbolinks-cache-control" content="no-cache">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
+        <link rel="icon" type="image/png" href="/favicon.png">
 
         <meta property="og:description" content="${options.description}">
         <meta property="og:image" content="${options.og.image}">
