2ae12f2f76569c883a20680fee2ee0b4f7791979
Simon Repp <simon@fdpl.io>
2019-08-20 10:59:39 +0200

    Refine footer desktop layout

--- urbanize.at/widgets/footer.js
+++ urbanize.at/widgets/footer.js
@@ -1,13 +1,13 @@
 module.exports = urbanize => `
   <div class="footer">
     <div class="alignment">
-      <div>
+      <div class="buttons">
         <a class="button_rect_black font_size_1_2" href="#">Newsletter</a><br>
         <a class="button_rect_black font_size_1_2" href="#">Impressum</a><br>
       </div>
 
       <div>
-        <img alt="Logo dérive" src="/images/derive.svg"><br>
+        <img class="derive_logo" alt="Logo dérive" src="/images/derive.svg"><br>
         <div class="font_size_1_3">dérive — Verein für Stadtforschung</div>
         <div class="font_size_1_3"><a href="https://derive.at">derive.at</a></div><br><br>
 
@@ -17,11 +17,12 @@ module.exports = urbanize => `
         <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
         <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
 
-        © 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN
+        <span class="font_size_0_8">© 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN</span>
       </div>
 
-      <div>
-        Festivalarchiv<br>
+      <div class="archive">
+        <div class="label font_size_1_3">Festivalarchiv</div>
+
         <a href="https://2018.urbanize.at/">ur9anize! 2018</a><br>
         <a href="https://2017.urbanize.at/">ur8anize! 2017</a><br>
         <a href="https://2016.urbanize.at/">urbani7e! 2016</a><br>
@@ -29,7 +30,7 @@ module.exports = urbanize => `
         <a href="https://2014.urbanize.at/">ur5anize! 2014</a><br>
         <a href="https://2013.urbanize.at/">ur4anize! 2013</a><br>
         <a href="https://2012.urbanize.at/">ur3anize! 2012</a><br>
-        <a href="https://2011.urbanize.at/">urbani2e! 2011</a><br>
+        <a href="https://2011.urbanize.at/">urbani2e! 2011</a>
       </div>
     </div>
   </div>`;
