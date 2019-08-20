e7571b64ed806e6674e587ead7f5d85ef0fd1b7d
Simon Repp <simon@fdpl.io>
2019-08-20 10:54:35 +0200

    Introduce generic font-size classes for terminal elements

--- urbanize.at/widgets/footer.js
+++ urbanize.at/widgets/footer.js
@@ -2,20 +2,20 @@ module.exports = urbanize => `
   <div class="footer">
     <div class="alignment">
       <div>
-        <a class="button_rect_black" href="#">Newsletter</a><br>
-        <a class="button_rect_black" href="#">Impressum</a><br>
+        <a class="button_rect_black font_size_1_2" href="#">Newsletter</a><br>
+        <a class="button_rect_black font_size_1_2" href="#">Impressum</a><br>
       </div>
 
       <div>
         <img alt="Logo dérive" src="/images/derive.svg"><br>
-        dérive — Verein für Stadtforschung<br>
-        <a href="https://derive.at">derive.at</a><br><br>
+        <div class="font_size_1_3">dérive — Verein für Stadtforschung</div>
+        <div class="font_size_1_3"><a href="https://derive.at">derive.at</a></div><br><br>
 
-        <a class="button_circle_black" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
-        <a class="button_circle_black" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
-        <a class="button_circle_black" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
-        <a class="button_circle_black" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
-        <a class="button_circle_black" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
+        <a class="button_circle_black font_size_1_6" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
+        <a class="button_circle_black font_size_1_6" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
+        <a class="button_circle_black font_size_1_6" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
+        <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
+        <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
 
         © 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN
       </div>
--- urbanize.at/widgets/header.js
+++ urbanize.at/widgets/header.js
@@ -2,20 +2,20 @@ module.exports = urbanize => `
   <div class="header">
     <div class="alignment">
       <span class="hover_menu_trigger">
-        <a class="button_rect_white" href="/">Festival</a>
+        <a class="button_rect_white font_size_1_2" href="/">Festival</a>
         <div class="hover_menu">
-          <a class="button_rect_white" href="/about/">About</a>
-          <a class="button_rect_white" href="/alltagsforschung/">Alltagsforschung</a>
-          <a class="button_rect_white" href="/stadtlabor/">Stadtlabor</a>
-          <a class="button_rect_white" href="/orte/">Orte</a>
-          <a class="button_rect_white" href="/partnerinnen/">PartnerInnen</a>
+          <a class="button_rect_white font_size_1_2" href="/about/">About</a>
+          <a class="button_rect_white font_size_1_2" href="/alltagsforschung/">Alltagsforschung</a>
+          <a class="button_rect_white font_size_1_2" href="/stadtlabor/">Stadtlabor</a>
+          <a class="button_rect_white font_size_1_2" href="/orte/">Orte</a>
+          <a class="button_rect_white font_size_1_2" href="/partnerinnen/">PartnerInnen</a>
         </div>
       </span>
-      <a class="button_rect_white" href="/programm/">Programm</a>
-      <a class="button_rect_white" href="/beteiligte/">Beteiligte</a>
-      <a class="button_rect_white" href="/presse/">Presse</a>
-      <a class="button_rect_white" href="/kontakt/">Kontakt</a>
-      <a class="language" href="/english/">EN</a>
+      <a class="button_rect_white font_size_1_2" href="/programm/">Programm</a>
+      <a class="button_rect_white font_size_1_2" href="/beteiligte/">Beteiligte</a>
+      <a class="button_rect_white font_size_1_2" href="/presse/">Presse</a>
+      <a class="button_rect_white font_size_1_2" href="/kontakt/">Kontakt</a>
+      <a class="language font_size_1_2" href="/english/">EN</a>
     </div>
   </div>
 `;
