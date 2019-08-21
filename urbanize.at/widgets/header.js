59e6f46d03cb5fb5d29255baf44e0e1f6f1c3c2d
Simon Repp <simon@fdpl.io>
2019-08-21 11:26:34 +0200

    Introduce responsive header release candidate

--- urbanize.at/widgets/header.js
+++ urbanize.at/widgets/header.js
@@ -1,6 +1,14 @@
 module.exports = urbanize => `
+  <div class="dim_overlay"
+       onclick="
+         document.querySelector('.toggle_menu').classList.toggle('visible');
+         document.querySelector('.dim_overlay').classList.toggle('visible');
+         return false;
+       ">
+  </div>
+
   <div class="header">
-    <div class="alignment">
+    <div class="alignment_desktop">
       <span class="hover_menu_trigger">
         <a class="button_rect_white font_size_1_2" href="/">Festival</a>
         <div class="hover_menu">
@@ -14,6 +22,32 @@ module.exports = urbanize => `
       <a class="button_rect_white font_size_1_2" href="/presse/">Presse</a>
       <a class="button_rect_white font_size_1_2" href="/kontakt/">Kontakt</a>
       <a class="language font_size_1_2" href="/english/">EN</a>
+    </div>
+
+    <div class="alignment_mobile">
+      <a class="button_circle_black font_size_1_6"
+         href="#"
+         onclick="
+          document.querySelector('.toggle_menu').classList.toggle('visible');
+          document.querySelector('.dim_overlay').classList.toggle('visible');
+          return false;
+         ">
+        <span class="icon-menu"></span>
+      </a>
+
+      <div class="toggle_menu">
+        <a class="button_rect_white font_size_1_2" href="/">Festival</a>
+        <div class="indented">
+          <a class="button_rect_yellow font_size_1_2" href="/about/">About</a>
+          <a class="button_rect_yellow font_size_1_2" href="/orte/">Orte</a>
+          <a class="button_rect_yellow font_size_1_2" href="/partnerinnen/">PartnerInnen</a>
+        </div>
+        <a class="button_rect_white font_size_1_2" href="/programm/">Programm</a>
+        <a class="button_rect_white font_size_1_2" href="/beteiligte/">Beteiligte</a>
+        <a class="button_rect_white font_size_1_2" href="/presse/">Presse</a>
+        <a class="button_rect_white font_size_1_2" href="/kontakt/">Kontakt</a>
+        <a class="button_rect_white font_size_1_2" href="/english/">English</a>
+      </div>
     </div>
   </div>
 `;
