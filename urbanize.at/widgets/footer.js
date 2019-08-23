b367828a36cdd4345d8aa285f1419f528593d2fe
Simon Repp <simon@fdpl.io>
2019-08-23 16:17:47 +0200

    Open external footer links in new tabs

--- urbanize.at/widgets/footer.js
+++ urbanize.at/widgets/footer.js
@@ -3,11 +3,11 @@ const DERIVE_INFO = `
   <div class="font_size_1_3">dérive — Verein für Stadtforschung</div>
   <div class="font_size_1_3"><a href="https://derive.at">derive.at</a></div><br><br>
 
-  <a class="button_circle_black font_size_1_6" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
-  <a class="button_circle_black font_size_1_6" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
-  <a class="button_circle_black font_size_1_6" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
-  <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
-  <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
+  <a class="button_circle_black font_size_1_6" href="https://facebook.com/derivemagazin/" target="_blank"><span class="icon-facebook"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://twitter.com/derivemagazin" target="_blank"><span class="icon-twitter"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://instagram.com/derive_urbanize/" target="_blank"><span class="icon-instagram"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung" target="_blank"><span class="icon-vimeo"></span></a>
+  <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin" target="_blank"><span class="icon-flickr"></span></a><br><br>
 
   <span class="font_size_0_8">© 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN</span>
 `;
@@ -27,14 +27,14 @@ module.exports = urbanize => `
       <div class="archive">
         <div class="label font_size_1_3">Festivalarchiv</div>
 
-        <a href="https://2018.urbanize.at/">ur9anize! 2018</a><br>
-        <a href="https://2017.urbanize.at/">ur8anize! 2017</a><br>
-        <a href="https://2016.urbanize.at/">urbani7e! 2016</a><br>
-        <a href="https://2015.urbanize.at/">ur6anize! 2015</a><br>
-        <a href="https://2014.urbanize.at/">ur5anize! 2014</a><br>
-        <a href="https://2013.urbanize.at/">ur4anize! 2013</a><br>
-        <a href="https://2012.urbanize.at/">ur3anize! 2012</a><br>
-        <a href="https://2011.urbanize.at/">urbani2e! 2011</a>
+        <a href="https://2018.urbanize.at/" target="_blank">ur9anize! 2018</a><br>
+        <a href="https://2017.urbanize.at/" target="_blank">ur8anize! 2017</a><br>
+        <a href="https://2016.urbanize.at/" target="_blank">urbani7e! 2016</a><br>
+        <a href="https://2015.urbanize.at/" target="_blank">ur6anize! 2015</a><br>
+        <a href="https://2014.urbanize.at/" target="_blank">ur5anize! 2014</a><br>
+        <a href="https://2013.urbanize.at/" target="_blank">ur4anize! 2013</a><br>
+        <a href="https://2012.urbanize.at/" target="_blank">ur3anize! 2012</a><br>
+        <a href="https://2011.urbanize.at/" target="_blank">urbani2e! 2011</a>
       </div>
     </div>
 
