557ee81e637585b95dcb34339dcf68cbbe86ba99
Simon Repp <simon@fdpl.io>
2019-08-23 15:11:46 +0200

    Add responsive footer behavior

--- urbanize.at/widgets/footer.js
+++ urbanize.at/widgets/footer.js
@@ -1,3 +1,17 @@
+const DERIVE_INFO = `
+  <img class="derive_logo" alt="Logo dérive" src="/images/derive.svg"><br>
+  <div class="font_size_1_3">dérive — Verein für Stadtforschung</div>
+  <div class="font_size_1_3"><a href="https://derive.at">derive.at</a></div><br><br>
+
+  <a class="button_circle_black font_size_1_6" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
+  <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
+  <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
+
+  <span class="font_size_0_8">© 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN</span>
+`;
+
 module.exports = urbanize => `
   <div class="footer">
     <div class="alignment">
@@ -6,18 +20,8 @@ module.exports = urbanize => `
         <a class="button_rect_black font_size_1_2" href="/impressum/">Impressum</a><br>
       </div>
 
-      <div>
-        <img class="derive_logo" alt="Logo dérive" src="/images/derive.svg"><br>
-        <div class="font_size_1_3">dérive — Verein für Stadtforschung</div>
-        <div class="font_size_1_3"><a href="https://derive.at">derive.at</a></div><br><br>
-
-        <a class="button_circle_black font_size_1_6" href="https://facebook.com/derivemagazin/"><span class="icon-facebook"></span></a>
-        <a class="button_circle_black font_size_1_6" href="https://twitter.com/derivemagazin"><span class="icon-twitter"></span></a>
-        <a class="button_circle_black font_size_1_6" href="https://instagram.com/derive_urbanize/"><span class="icon-instagram"></span></a>
-        <a class="button_circle_black font_size_1_6" href="https://vimeo.com/derivestadtforschung"><span class="icon-vimeo"></span></a>
-        <a class="button_circle_black font_size_1_6" href="http://flickr.com/photos/derivemagazin"><span class="icon-flickr"></span></a><br><br>
-
-        <span class="font_size_0_8">© 2019 URBANIZE! INTERNATIONALES FESTIVAL FÜR URBANE ERKUNDUNGEN WIEN</span>
+      <div class="derive_desktop">
+        ${DERIVE_INFO}
       </div>
 
       <div class="archive">
@@ -32,5 +36,9 @@ module.exports = urbanize => `
         <a href="https://2012.urbanize.at/">ur3anize! 2012</a><br>
         <a href="https://2011.urbanize.at/">urbani2e! 2011</a>
       </div>
+    </div>
+
+    <div class="derive_mobile">
+      ${DERIVE_INFO}
     </div>
   </div>`;
