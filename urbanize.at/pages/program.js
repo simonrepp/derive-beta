137eed6e2f877d3c3ad7900c7cd0ab053b0f7361
Simon Repp <simon@fdpl.io>
2019-09-15 15:05:32 +0200

    Draft program filtering for dates and categories

--- urbanize.at/pages/program.js
+++ urbanize.at/pages/program.js
@@ -5,13 +5,52 @@ const scrollToTop = require('../widgets/scroll_to_top.js');
 module.exports = urbanize => {
   const html = `
     <div>
-      <h1>
-        Programm
-      </h1>
+      <h2>
+        Filtern nach
+      </h2>
 
-      <a class="button_rect_pink filter" data-filter="date">Datum</a> &nbsp;&nbsp;
-      <a class="button_rect_pink filter" data-filter="category">Kategorie</a> &nbsp;&nbsp;
-      <a class="button_rect_pink filter" data-filter="query">Volltextsuche</a> &nbsp;&nbsp;
+      <script type="text/javascript">
+        function filterEvents(type, value) {
+          for(const event of document.querySelectorAll('.event_filterable')) {
+            if(type === 'date') {
+              event.style.display = event.dataset.date === value ? 'block' : 'none';
+            }
+
+            if(type === 'category') {
+              event.style.display = event.dataset.category === value ? 'block' : 'none';
+            }
+          }
+        };
+      </script>
+
+      <span class="hover_menu_trigger">
+        <a class="button_rect_pink" data-filter="date" href="/">Datum</a>
+        <div class="hover_menu">
+          <a class="button_rect_yellow" data-date="2019-10-09" href="#" onclick="filterEvents('date', this.dataset.date); return false;">9 OKT 2019</a>
+          <a class="button_rect_yellow" data-date="2019-10-10" href="#" onclick="filterEvents('date', this.dataset.date); return false;">10 OKT 2019</a>
+          <a class="button_rect_yellow" data-date="2019-10-11" href="#" onclick="filterEvents('date', this.dataset.date); return false;">11 OKT 2019</a>
+          <a class="button_rect_yellow" data-date="2019-10-12" href="#" onclick="filterEvents('date', this.dataset.date); return false;">12 OKT 2019</a>
+          <a class="button_rect_yellow" data-date="2019-10-13" href="#" onclick="filterEvents('date', this.dataset.date); return false;">13 OKT 2019</a>
+        </div>
+      </span> &nbsp;&nbsp;
+
+      <span class="hover_menu_trigger">
+        <a class="button_rect_pink" data-filter="category" href="/">Kategorie</a>
+        <div class="hover_menu">
+          <a class="button_rect_yellow" data-category="Film-Kunst-Musik" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Film-Kunst-Musik</a>
+          <a class="button_rect_yellow" data-category="Stadt-Praxis" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Stadt-Praxis</a>
+          <a class="button_rect_yellow" data-category="Vortrag/Diskussion" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Vortrag/Diskussion</a>
+          <a class="button_rect_yellow" data-category="Workshop" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Workshop</a>
+        </div>
+      </span> &nbsp;&nbsp;
+
+      <span class="hover_menu_trigger">
+        <a class="button_rect_pink" data-filter="query" href="/">Volltextsuche</a>
+        <div class="hover_menu">
+          <input type="search">
+          <a class="button_rect_pink" href="/search-todo/">Suchen</a>
+        </div>
+      </span>
 
       <br><br>
 
--- urbanize.at/widgets/event-listing.js
+++ urbanize.at/widgets/event-listing.js
@@ -33,28 +33,9 @@ module.exports = events => {
   return `
     <div>
       ${sortedEvents.map(({ date, event }) => `
-        <div class="event margin_y_2_0">
-          <div class="event_image_desktop">
-            ${event.image ? `
-              <img src="${event.image.written}">
-            `:''}
-            ${event.imageCredits ? `
-              <div class="color_grey font_size_0_8 text_align_right">${event.imageCredits}</div>
-            `:''}
-          </div>
-
-          <div class="event_details">
-            <!-- position: absolute, right: 0 etc. -->
-            <a class="button_rect_black event_category_tag" href="#">
-              ${event.category}
-            </a>
-
-            <strong class="color_pink">${moment(date.date).locale('de').format('dd, D MMM YYYY')}</strong><br>
-            <strong class="color_pink">${date.time.raw}</strong>
-
-            <hr class="hairline">
-
-            <div class="event_image_mobile">
+        <div class="event_filterable" data-category="${event.category}" data-date="${moment(date.date).format('YYYY-MM-DD')}">
+          <div class="event margin_y_2_0">
+            <div class="event_image_desktop">
               ${event.image ? `
                 <img src="${event.image.written}">
               `:''}
@@ -63,77 +44,98 @@ module.exports = events => {
               `:''}
             </div>
 
-            <strong>${event.venue}</strong><br>
-            <strong>
-              ${event.mapLink ? `<a href="${event.mapLink}" target="_blank">${event.address}</a>` : event.address}
-            </strong>
-
-            ${event.directions ? `
-              <div class="margin_y_0_5">
-                ${event.directions.converted}
-              </div>
-            ` : ''}
-
-            <hr class="hairline">
-
-            <h2>
-              <a href="/${event.permalink}/">
-                ${event.title}
+            <div class="event_details">
+              <!-- TODO: Category tag button behavior? -->
+              <a class="button_rect_black event_category_tag" href="#">
+                ${event.category}
               </a>
-            </h2>
 
-            <div class="margin_y_0_5">
-              ${event.abstract.converted.replace(/(?=<\/p>\s*$)/, `
+              <strong class="color_pink">${moment(date.date).locale('de').format('dd, D MMM YYYY')}</strong><br>
+              <strong class="color_pink">${date.time.raw}</strong>
+
+              <hr class="hairline">
+
+              <div class="event_image_mobile">
+                ${event.image ? `
+                  <img src="${event.image.written}">
+                `:''}
+                ${event.imageCredits ? `
+                  <div class="color_grey font_size_0_8 text_align_right">${event.imageCredits}</div>
+                `:''}
+              </div>
+
+              <strong>${event.venue}</strong><br>
+              <strong>
+                ${event.mapLink ? `<a href="${event.mapLink}" target="_blank">${event.address}</a>` : event.address}
+              </strong>
+
+              ${event.directions ? `
+                <div class="margin_y_0_5">
+                  ${event.directions.converted}
+                </div>
+              ` : ''}
+
+              <hr class="hairline">
+
+              <h2>
                 <a href="/${event.permalink}/">
-                  <img src="/images/arrow.svg">
+                  ${event.title}
                 </a>
-              `)}
-            </div>
+              </h2>
 
-            ${event.participants.length > 0 ? `
               <div class="margin_y_0_5">
-                <strong>
-                  Mit
-                  <a href="/${event.participants[0].permalink}/" target="_blank">
-                    ${event.participants[0].name}
+                ${event.abstract.converted.replace(/(?=<\/p>\s*$)/, `
+                  <a href="/${event.permalink}/">
+                    <img src="/images/arrow.svg">
                   </a>
+                `)}
+              </div>
 
-                  ${event.participants >= 3 ? event.participants.slice(1, event.participants.length - 1).map(participant => `
-                    ,
-                    <a href="/${participant.permalink}/" target="_blank">
-                      ${participant.name}
+              ${event.participants.length > 0 ? `
+                <div class="margin_y_0_5">
+                  <strong>
+                    Mit
+                    <a href="/${event.participants[0].permalink}/" target="_blank">
+                      ${event.participants[0].name}
                     </a>
-                  `).join('') : ''}
 
-                  ${event.participants >= 2 ? `
-                    und
-                    <a href="/${event.participants[event.participants.length - 1].permalink}/" target="_blank">
-                      ${event.participants[event.participants.length - 1].name}
-                    </a>
-                  ` : ''}
-                </strong>
-              </div>
-            ` : ''}
+                    ${event.participants >= 3 ? event.participants.slice(1, event.participants.length - 1).map(participant => `
+                      ,
+                      <a href="/${participant.permalink}/" target="_blank">
+                        ${participant.name}
+                      </a>
+                    `).join('') : ''}
 
-            ${event.additionalInfo ? `
-              <div class="margin_y_0_5">
-                ${event.additionalInfo.converted}
-              </div>
-            ` : ''}
+                    ${event.participants >= 2 ? `
+                      und
+                      <a href="/${event.participants[event.participants.length - 1].permalink}/" target="_blank">
+                        ${event.participants[event.participants.length - 1].name}
+                      </a>
+                    ` : ''}
+                  </strong>
+                </div>
+              ` : ''}
 
-            <div class="flex_split_lr margin_y_0_5">
-              <div>
-                ${calendarButton(event, date)}
-              </div>
+              ${event.additionalInfo ? `
+                <div class="margin_y_0_5">
+                  ${event.additionalInfo.converted}
+                </div>
+              ` : ''}
 
-              <div>
-                ${signupButton(event, date)}
+              <div class="flex_split_lr margin_y_0_5">
+                <div>
+                  ${calendarButton(event, date)}
+                </div>
+
+                <div>
+                  ${signupButton(event, date)}
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <hr>
+          <hr>
+        </div>
       `).join('')}
     </div>
   `;
