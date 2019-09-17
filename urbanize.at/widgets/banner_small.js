73ae7c386294a375cb64a2a0236ae501e71aacf4
Simon Repp <simon@fdpl.io>
2019-09-17 16:07:41 +0200

    Link small urbanize banner logo to home

--- urbanize.at/widgets/banner_small.js
+++ urbanize.at/widgets/banner_small.js
@@ -1,7 +1,9 @@
 module.exports = variant => `
   <div>
     <div class="banner_small_${variant}">
-      <img class="logo" src="/images/urbanize_logo_${variant}.svg">
+      <a href="/">
+        <img class="logo" src="/images/urbanize_logo_${variant}.svg">
+      </a>
       <img class="slogan" src="/images/alle_tage_wohnungsfrage_${variant}.svg">
       <div class="info font_size_1_2 font_weight_500">
         9—13 OKT 2019<br>
