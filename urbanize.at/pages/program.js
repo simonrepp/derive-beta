808a6bc7f824a81c0e65783afbf82a1117296089
Simon Repp <simon@fdpl.io>
2019-09-18 11:25:32 +0200

    Add properly functional combined date and category program filtering

--- urbanize.at/pages/program.js
+++ urbanize.at/pages/program.js
@@ -10,47 +10,90 @@ module.exports = urbanize => {
       </h2>
 
       <script type="text/javascript">
-        function filterEvents(type, value) {
+        function filter() {
+          const activeDate = document.querySelector('.active_date');
+          const activeCategory = document.querySelector('.active_category');
+
           for(const event of document.querySelectorAll('.event_filterable')) {
-            if(type === 'date') {
-              event.style.display = event.dataset.date === value ? 'block' : 'none';
+            let display = true;
+
+            if(activeDate.dataset.value &&
+               activeDate.dataset.value !== 'all' &&
+               event.dataset.date !== activeDate.dataset.value) {
+              display = false;
             }
 
-            if(type === 'category') {
-              event.style.display = event.dataset.category === value ? 'block' : 'none';
+            if(activeCategory.dataset.value &&
+               activeCategory.dataset.value !== 'all' &&
+               event.dataset.category !== activeCategory.dataset.value) {
+              display = false;
+            }
+
+            event.style.display = display ? 'block' : 'none';
+          }
+        }
+
+        function updateFilter(option) {
+          if(option.dataset.date) {
+            const activeDate = document.querySelector('.active_date');
+
+            activeDate.dataset.value = option.dataset.date;
+
+            if(option.dataset.date === 'all') {
+              activeDate.innerHTML = '';
+            } else {
+              activeDate.innerHTML = ': ' + option.innerHTML;
             }
           }
-        };
+
+          if(option.dataset.category) {
+            const activeCategory = document.querySelector('.active_category');
+
+            activeCategory.dataset.value = option.dataset.category;
+
+            if(option.dataset.category === 'all') {
+              activeCategory.innerHTML = '';
+            } else {
+              activeCategory.innerHTML = ': ' + option.innerHTML;
+            }
+          }
+
+          filter();
+        }
       </script>
 
-      <span class="hover_menu_trigger">
-        <a class="button_rect_pink" data-filter="date" href="/">Datum</a>
-        <div class="hover_menu">
-          <a class="button_rect_yellow" data-date="2019-10-09" href="#" onclick="filterEvents('date', this.dataset.date); return false;">9 OKT 2019</a>
-          <a class="button_rect_yellow" data-date="2019-10-10" href="#" onclick="filterEvents('date', this.dataset.date); return false;">10 OKT 2019</a>
-          <a class="button_rect_yellow" data-date="2019-10-11" href="#" onclick="filterEvents('date', this.dataset.date); return false;">11 OKT 2019</a>
-          <a class="button_rect_yellow" data-date="2019-10-12" href="#" onclick="filterEvents('date', this.dataset.date); return false;">12 OKT 2019</a>
-          <a class="button_rect_yellow" data-date="2019-10-13" href="#" onclick="filterEvents('date', this.dataset.date); return false;">13 OKT 2019</a>
-        </div>
-      </span> &nbsp;&nbsp;
+      <div class="filter_alignment">
+        <span class="hover_menu_trigger">
+          <a class="button_rect_pink" data-filter="date" href="#" onclick="return false;">Datum<span class="active_date"></span></a>
+          <div class="hover_menu">
+            <a class="button_rect_yellow" data-date="all" href="#" onclick="updateFilter(this); return false;">Alle Tage</a>
+            <a class="button_rect_yellow" data-date="2019-10-09" href="#" onclick="updateFilter(this); return false;">9 OKT 2019</a>
+            <a class="button_rect_yellow" data-date="2019-10-10" href="#" onclick="updateFilter(this); return false;">10 OKT 2019</a>
+            <a class="button_rect_yellow" data-date="2019-10-11" href="#" onclick="updateFilter(this); return false;">11 OKT 2019</a>
+            <a class="button_rect_yellow" data-date="2019-10-12" href="#" onclick="updateFilter(this); return false;">12 OKT 2019</a>
+            <a class="button_rect_yellow" data-date="2019-10-13" href="#" onclick="updateFilter(this); return false;">13 OKT 2019</a>
+          </div>
+        </span>
 
-      <span class="hover_menu_trigger">
-        <a class="button_rect_pink" data-filter="category" href="/">Kategorie</a>
-        <div class="hover_menu">
-          <a class="button_rect_yellow" data-category="Film-Kunst-Musik" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Film-Kunst-Musik</a>
-          <a class="button_rect_yellow" data-category="Stadt-Praxis" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Stadt-Praxis</a>
-          <a class="button_rect_yellow" data-category="Vortrag/Diskussion" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Vortrag/Diskussion</a>
-          <a class="button_rect_yellow" data-category="Workshop" href="#" onclick="filterEvents('category', this.dataset.category); return false;">Workshop</a>
-        </div>
-      </span> &nbsp;&nbsp;
+        <span class="hover_menu_trigger">
+          <a class="button_rect_pink" data-filter="category" href="#" onclick="return false;">Kategorie<span class="active_category"></span></a>
+          <div class="hover_menu">
+            <a class="button_rect_yellow" data-category="all" href="#" onclick="updateFilter(this); return false;">Alle Kategorien</a>
+            <a class="button_rect_yellow" data-category="Film-Kunst-Musik" href="#" onclick="updateFilter(this); return false;">Film-Kunst-Musik</a>
+            <a class="button_rect_yellow" data-category="Stadt-Praxis" href="#" onclick="updateFilter(this); return false;">Stadt-Praxis</a>
+            <a class="button_rect_yellow" data-category="Vortrag/Diskussion" href="#" onclick="updateFilter(this); return false;">Vortrag/Diskussion</a>
+            <a class="button_rect_yellow" data-category="Workshop" href="#" onclick="updateFilter(this); return false;">Workshop</a>
+          </div>
+        </span>
 
-      <span class="hover_menu_trigger">
-        <a class="button_rect_pink" data-filter="query" href="/">Volltextsuche</a>
-        <div class="hover_menu">
-          <input type="search">
-          <a class="button_rect_pink" href="/search-todo/">Suchen</a>
-        </div>
-      </span>
+        <!-- span class="hover_menu_trigger">
+          <a class="button_rect_pink" data-filter="query" href="/">Volltextsuche</a>
+          <div class="hover_menu">
+            <input type="search">
+            <a class="button_rect_pink" href="/search-todo/">Suchen</a>
+          </div>
+        </span -->
+      </div>
 
       <br><br>
 
