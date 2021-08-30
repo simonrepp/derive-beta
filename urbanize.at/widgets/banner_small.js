115b0587e61ec3d1d59d9f461eccac476d094b4a
Simon Repp <simon@fdpl.io>
2021-08-30 11:41:20 +0200

    Sujet changes for 2021

--- urbanize.at/widgets/banner_small.js
+++ urbanize.at/widgets/banner_small.js
@@ -5,7 +5,7 @@ module.exports = variant => `
         <div class="logo" style="background-image: url('/images/urbanize_sprite_${variant}.svg?final');"></div>
         <img class="tagline" src="/images/tagline_${variant}.svg">
       </a>
-      <img class="slogan" src="/images/common_spaces_hybrid_places_${variant === 'white' ? 'yellow' : variant}.svg">
+      <img class="slogan" src="/images/strategien_des_wandels_${variant === 'white' ? 'yellow' : variant}.svg">
       <div class="info font_size_1_2 font_weight_500">
         06—10 OKT 2021<br>
         Wien—Nordwestbahnhof
