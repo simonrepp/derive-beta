799bda5d77c71b15448355a6654137b38903b085
Simon Repp <simon@fdpl.io>
2021-09-09 11:21:23 +0200

    Switch to static urbanize logo, indentation changes

--- urbanize.at/widgets/banner_large.js
+++ urbanize.at/widgets/banner_large.js
@@ -1,15 +1,14 @@
 module.exports = `
-  <div class="banner_large">
-    <div class="alignment">
-      <div class="logo_wrapper">
-        <div class="logo"></div>
-        <img class="tagline" src="/images/tagline_white.svg">
-      </div>
-      <div class="info font_weight_500">
-        06—10 OKT 2021<br>
-        Wien—Nordwestbahnhof
-      </div>
-      <img class="sujet" src="/images/sujet.png">
+    <div class="banner_large">
+        <div class="alignment">
+            <div class="logo_wrapper">
+                <img class="logo_static" src="/images/urbanize_static_white.svg">
+            </div>
+            <div class="info font_weight_500">
+                06—10 OKT 2021<br>
+                Wien—Nordwestbahnhof
+            </div>
+            <img class="sujet" src="/images/sujet.png">
+        </div>
     </div>
-  </div>
 `;
--- urbanize.at/widgets/banner_small.js
+++ urbanize.at/widgets/banner_small.js
@@ -1,15 +1,14 @@
 module.exports = variant => `
-  <div>
-    <div class="banner_small banner_small_${variant}">
-      <a class="logo_wrapper" href="/">
-        <div class="logo" style="background-image: url('/images/urbanize_sprite_${variant}.svg?final');"></div>
-        <img class="tagline" src="/images/tagline_${variant}.svg">
-      </a>
-      <img class="slogan" src="/images/strategien_des_wandels_${variant === 'white' ? 'yellow' : variant}.svg">
-      <div class="info font_size_1_2 font_weight_500">
-        06—10 OKT 2021<br>
-        Wien—Nordwestbahnhof
-      </div>
+    <div>
+        <div class="banner_small banner_small_${variant}">
+            <a class="logo_wrapper" href="/">
+                <img class="logo_static" src="/images/urbanize_static_${variant}.svg">
+            </a>
+            <img class="slogan" src="/images/strategien_des_wandels_${variant === 'white' ? 'yellow' : variant}.svg">
+            <div class="info font_size_1_2 font_weight_500">
+                06—10 OKT 2021<br>
+                Wien—Nordwestbahnhof
+            </div>
+        </div>
     </div>
-  </div>
 `;
