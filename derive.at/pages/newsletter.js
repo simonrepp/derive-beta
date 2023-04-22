00ca243535516168e009bc3e9208c76dbe64d7bd
Simon Repp <simon@fdpl.io>
2023-04-22 19:02:45 +0200

    Update newsletter after mailjet introducing breaking changes

--- derive.at/pages/newsletter.js
+++ derive.at/pages/newsletter.js
@@ -1,114 +1,42 @@
 const layout = require('./layout.js');
 
-// TODO: Replace current history step (= newsletter adress) with derive index on submit, so back button on next page leads to index instead of this page again
+// Newsletter Anmeldung
+//
+// E-Mail (email)
+// z.b. alice@example.com
+//
+// Vorname (firstName)
+// z.b. Alice
+//
+// Nachname (name)
+// z.b. Smith
+// 
+// Institution (institution)
+// Optional, z.b. Universität Wien
+// 
+// Stadt/Ort (stadt)
+// z.b. Wien
+// 
+// Land (country)
+// z.b. Österreich
+//
+// [ ] Ich stimme dem Erhalt dieses Newsletters zu und weiß, dass ich mich jederzeit problemlos abmelden kann. 
+//
+// Wir informieren mit unserem Newsletter ca. 1 - 2 x pro Monat über unsere Aktivitäten (Zeitschrift, Radio und URBANIZE!-Festival). Nach dem Wohnort/Land fragen wir, weil es uns ermöglicht z.B. Veranstaltungstipps gezielt an die Bewohner und Bewohnerinnen der jeweiligen Stadt/des Landes zu schicken. Der Newsletter kann jederzeit einfach abbestellt werden.
+//
+// Abonnieren
 
-const NEWSLETTER = {
-  csrfToken: 'MGJmZWQwYzE1Y2I0ZWEzNjkyZGQ3YjE5MWY5MGZjYzNmNTNiNTIzMGZjZTI2MmJlYmJlZTU0MjA1MmFlYTFhNw==',
-  keys: {
-    city: 'w-field-field-78037-434182-537738-125386',
-    country: 'w-field-field-78037-434182-537738-106942',
-    email: 'w-field-field-78037-434182-537738-email',
-    firstName: 'w-field-field-78037-434182-537738-106940',
-    institution: 'w-field-field-78037-434182-537738-125387',
-    lastName: 'w-field-field-78037-434182-537738-106941'
-  },
-  title: 'Newsletter Anmeldung',
-  url: 'https://app.mailjet.com/widget/iframe/2fTc/kiF'
-};
+const TITLE = 'Newsletter Anmeldung'
 
 module.exports = data => {
-  const html = `
-    <h1>
-      ${NEWSLETTER.title}
-    </h1>
+    const html = `
+        <h1>
+            ${TITLE}
+        </h1>
 
-    <form action="${NEWSLETTER.url}" method="post">
-      <input type="hidden"
-             id="csrf_token"
-             name="csrf_token"
-             value="${NEWSLETTER.csrfToken}">
+        <iframe data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://h8z6.mjt.lu/wgt/h8z6/5k1/form?c=2fe6f761" width="100%"></iframe>
+        <script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script>
+    `;
 
-      <div class="newsletter_label">E-Mail</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.email}"
-             placeholder="Z.b. john.smith@example.com"
-             required="required"
-             type="email">
-
-      <div class="newsletter_label">Vorname</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.firstName}"
-             placeholder="Z.b. John"
-             required="required"
-             type="text">
-
-      <div class="newsletter_label">Nachname</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.lastName}"
-             placeholder="Z.b. Smith"
-             required="required"
-             type="text">
-
-      <div class="newsletter_label">Institution</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.institution}"
-             placeholder="Optional, z.b. Universität Wien"
-             type="text">
-
-      <div class="newsletter_label">Stadt/Ort</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.city}"
-             placeholder="Z.b. Mannheim"
-             required="required"
-             type="text">
-
-      <div class="newsletter_label">Land</div>
-      <input class="newsletter_input"
-             name="${NEWSLETTER.keys.country}"
-             placeholder="Z.b. Schweiz"
-             required="required"
-             type="text">
-
-      <div class="newsletter_label">
-        <input class="newsletter_checkbox"
-               id="checkbox_consent"
-               name="w-preview-consent-checkbox"
-               required="required"
-               type="checkbox">
-
-        <span class="icon-checkbox"
-              onclick="
-                if(document.querySelector('#checkbox_consent').checked) {
-                  this.classList.remove('icon-checkbox-checked');
-                  this.classList.add('icon-checkbox');
-                  document.querySelector('#checkbox_consent').checked = false;
-                } else {
-                  this.classList.remove('icon-checkbox');
-                  this.classList.add('icon-checkbox-checked');
-                  document.querySelector('#checkbox_consent').checked = true;
-                }
-              ">
-        </span>
-
-        Ich stimme dem Erhalt dieses Newsletters zu und weiß, dass ich mich jederzeit problemlos abmelden kann.
-      </div>
-
-
-      <div class="newsletter_label">
-        Wir informieren mit unserem Newsletter ca. 1 - 2 x pro Monat über unsere
-        Aktivitäten (Zeitschrift, Radio und URBANIZE!-Festival). Nach dem Wohnort/Land
-        fragen wir, weil es uns ermöglicht z.B. Veranstaltungstipps gezielt an die
-        Bewohner und Bewohnerinnen der jeweiligen Stadt/des Landes zu schicken. Der
-        Newsletter kann jederzeit einfach abbestellt werden.
-      </div>
-
-      <button class="newsletter_button"
-              onclick="if(!document.querySelector('#checkbox_consent').checked) { alert('Bitte dem Erhalt des Newsletters durch Anklicken der Checkbox im Formular zustimmen.'); }"
-              type="submit">
-        Abonnieren
-      </button>
-    </form>
-  `;
-
-  return layout(data, html, { title: NEWSLETTER.title });
+    return layout(data, html, { title: TITLE });
 };
--- urbanize.at/pages/newsletter.js
+++ urbanize.at/pages/newsletter.js
@@ -1,89 +1,49 @@
 const layout = require('./layout.js');
 
-// TODO: Replace current history step (= newsletter adress) with derive index on submit, so back button on next page leads to index instead of this page again
+// Newsletter Anmeldung
+//
+// E-Mail (email)
+// z.b. alice@example.com
+//
+// Vorname (firstName)
+// z.b. Alice
+//
+// Nachname (name)
+// z.b. Smith
+// 
+// Institution (institution)
+// Optional, z.b. Universität Wien
+// 
+// Stadt/Ort (stadt)
+// z.b. Wien
+// 
+// Land (country)
+// z.b. Österreich
+//
+// [ ] Ich stimme dem Erhalt dieses Newsletters zu und weiß, dass ich mich jederzeit problemlos abmelden kann. 
+//
+// Wir informieren mit unserem Newsletter ca. 1 - 2 x pro Monat über unsere Aktivitäten (Zeitschrift, Radio und URBANIZE!-Festival). Nach dem Wohnort/Land fragen wir, weil es uns ermöglicht z.B. Veranstaltungstipps gezielt an die Bewohner und Bewohnerinnen der jeweiligen Stadt/des Landes zu schicken. Der Newsletter kann jederzeit einfach abbestellt werden.
+//
+// Abonnieren
 
 const PRESS_NEWSLETTER = {
-  csrfToken: 'MTRkYzVlNzI3Mjc0ODA1ODMzZTRjNzU3NGViYjI3NDg4MmVlN2U2NDU3MjllMGI0ZTM3OWE0YWUzMDFlMzUzYQ==',
-  keys: {
-    city: 'w-field-field-71835-403698-537738-125386',
-    country: 'w-field-field-71835-403698-537738-106942',
-    email: 'w-field-field-71835-403698-537738-email',
-    firstName: 'w-field-field-71835-403698-537738-106940',
-    institution: 'w-field-field-71835-403698-537738-125387',
-    lastName: 'w-field-field-71835-403698-537738-106941'
-  },
   title: 'Presse-Newsletter Anmeldung',
-  url: 'https://app.mailjet.com/widget/iframe/2fTc/iGD'
+  url: 'https://h8z6.mjt.lu/wgt/h8z6/5ng/form?c=75190393'
 };
 
 const PUBLIC_NEWSLETTER = {
-  csrfToken: 'MzAzYTQyMDVlM2ZhZGY4OWQzYWIxYTg0NDE1MTNmNWQwYWY1YWUxOGRhZGE5MDVmYTRkMmUyODk0ZGI2YzJhYw==',
-  keys: {
-    city: 'w-field-field-71620-402641-537738-125386',
-    country: 'w-field-field-71620-402641-537738-106942',
-    email: 'w-field-field-71620-402641-537738-email',
-    firstName: 'w-field-field-71620-402641-537738-106940',
-    institution: 'w-field-field-71620-402641-537738-125387',
-    lastName: 'w-field-field-71620-402641-537738-106941'
-  },
   title: 'Newsletter Anmeldung',
-  url: 'https://app.mailjet.com/widget/iframe/2fTc/iDa'
+  url: 'https://h8z6.mjt.lu/wgt/h8z6/5nv/form?c=0d026ce5'
 };
 
 module.exports = (urbanize, audience) => {
-  const newsletter = audience === 'press' ? PRESS_NEWSLETTER : PUBLIC_NEWSLETTER;
+    const newsletter = audience === 'press' ? PRESS_NEWSLETTER : PUBLIC_NEWSLETTER;
 
-  const html = `
-    <h1>
-      ${newsletter.title}
-    </h1>
+    const html = `
+        <h1>${newsletter.title}</h1>
+        <iframe data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="${newsletter.url}" width="100%"></iframe>
+        <script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script>
+    `.trim();
 
-    <form action="${newsletter.url}" method="post">
-      <input type="hidden" id="csrf_token" name="csrf_token" value="${newsletter.csrfToken}">
-
-      <div class="color_black margin_y_0_5">E-Mail</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.email}" required="required" type="email">
-
-      <div class="color_black margin_y_0_5">Vorname</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.firstName}" required="required" type="text">
-
-      <div class="color_black margin_y_0_5">Nachname</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.lastName}" required="required" type="text">
-
-      <div class="color_black margin_y_0_5">Institution</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.institution}" type="text">
-
-      <div class="color_black margin_y_0_5">Stadt/Ort</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.city}" required="required" type="text">
-
-      <div class="color_black margin_y_0_5">Land</div>
-      <input class="rounded_rect input_rect_white margin_y_0_5" name="${newsletter.keys.country}" required="required" type="text">
-
-      <div class="color_black margin_y_2_0">
-        <input id="checkbox_consent" name="w-preview-consent-checkbox" required="required" type="checkbox">
-        <label class="slider_white" for="checkbox_consent">
-          <span class="nub"></span>
-        </label>
-
-        Ich stimme dem Erhalt dieses Newsletters zu und weiß, dass ich mich jederzeit problemlos abmelden kann.
-      </div>
-
-
-      <div class="font_size_0_8 margin_y_2_0">
-        Wir informieren mit unserem Newsletter ca. 1 - 2 x pro Monat über unsere
-        Aktivitäten (Zeitschrift, Radio und URBANIZE!-Festival). Nach dem Wohnort/Land
-        fragen wir, weil es uns ermöglicht z.B. Veranstaltungstipps gezielt an die
-        Bewohner und Bewohnerinnen der jeweiligen Stadt/des Landes zu schicken. Der
-        Newsletter kann jederzeit einfach abbestellt werden.
-      </div>
-
-      <button class="rounded_rect button_rect_white margin_y_0_5"
-              onclick="if(!document.querySelector('#checkbox_consent').checked) { alert('Bitte dem Erhalt des Newsletters durch Aktivierung des Sliders im Formular zustimmen.'); }"
-              type="submit">
-        Abonnieren
-      </button>
-    </form>
-  `;
-
-  return layout(html, urbanize, { slim: true });
+    return layout(html, urbanize, { slim: true });
 };
