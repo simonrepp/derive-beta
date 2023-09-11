641ff7bd2ea02e6dfbfa10302b808dcabe02c1e5
Simon Repp <simon@fdpl.io>
2023-09-11 11:06:15 +0200

    Remove note that no signup is necessary

--- urbanize.at/widgets/signup_button.js
+++ urbanize.at/widgets/signup_button.js
@@ -1,6 +1,8 @@
 module.exports = (event, date) => {
   if(!event.signupLink)
-    return '<span class="color_accent">Keine Anmeldung notwendig</span>';
+    return '<span></span>';
+    // Removed this notice for urbanize 2023, keeping it for maybe bringing it back:
+    // return '<span class="color_accent">Keine Anmeldung notwendig</span>';
 
   if(date.fullyBooked)
     return '<span class="color_accent">Veranstaltung bereits ausgebucht</span>';
